83478e24bbd8022c668ea4d516bffa18f4e2e47d
chinthaka senarathna <chironprabath@gmail.com>
2024-09-11 18:38:20 +0530

    built the Java class which is represent in the Database -> Entity

--- src/main/java/com/example/E_Com/entity/Customer.java
+++ src/main/java/com/example/E_Com/entity/Customer.java
@@ -1,5 +1,6 @@
 package com.example.E_Com.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
@@ -13,10 +14,22 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity( name = "customer" )
 public class Customer {
+    @Id
+    @Column( name = "property_id", length = 80, nullable = false )
     private String propertyId;
+
+    @Column( name = "name", length = 50, nullable = false )
     private String name;
+
+    @Column( name = "email",length = 100, nullable = false, unique = true )
     private String email;
+
+    @Column( name = "phoneNo",length = 20, nullable = false)
     private String phoneNo;
+
+    @Column( name = "address", length = 255, nullable = false) // length = 255 is default
     private String address;
+
+    @Column( name = "is_active", columnDefinition = "TINYINT")
     private boolean isActive;
 }
