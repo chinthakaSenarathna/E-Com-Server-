d811a68b4ddce34f2f4b1bb77c7ffe8eb9b1f9f8
chinthaka senarathna <chironprabath@gmail.com>
2024-09-11 19:19:42 +0530

    built the Customer Service

--- src/main/java/com/example/E_Com/repo/customerRepository.java
+++ src/main/java/com/example/E_Com/repo/customerRepository.java
@@ -3,6 +3,5 @@ package com.example.E_Com.repo;
 import com.example.E_Com.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface customerRepository extends JpaRepository<Customer, String> {       // <Entity, Data type of primary key>
-    
+public interface CustomerRepository extends JpaRepository<Customer, String> {
 }
---
+++ src/main/java/com/example/E_Com/service/customerService.java
@@ -0,0 +1,7 @@
+package com.example.E_Com.service;
+
+import com.example.E_Com.dto.request.RequestCustomerDto;
+
+public interface CustomerService {
+    public void create(RequestCustomerDto requestCustomerDto);
+}
