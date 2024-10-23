41ec7c0486f9ec9f3aa2f08c5522ab69957314dd
chinthaka senarathna <chironprabath@gmail.com>
2024-10-23 17:32:05 +0530

    when Role assign to User, get the Permissions according to the Role ?

--- src/main/java/com/example/E_Com/security/ApplicationUserRole.java
+++ src/main/java/com/example/E_Com/security/ApplicationUserRole.java
@@ -1,12 +1,22 @@
 package com.example.E_com.security;
 
+import com.google.common.collect.Sets;
+
 import java.util.Set;
 
 public enum ApplicationUserRole {
-    USER(),
-    CUSTOMER(),
-    ADMIN(),
-    MANAGER();
+    USER(Sets.newHashSet()),
+    CUSTOMER(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet()),
+    MANAGER(Sets.newHashSet());
 
     private final Set<ApplicationUserPermission> applicationUserPermissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> applicationUserPermissions) {
+        this.applicationUserPermissions = applicationUserPermissions;
+    }
+
+    public Set<ApplicationUserPermission> getApplicationUserPermissions(){
+        return applicationUserPermissions;
+    }
 }
