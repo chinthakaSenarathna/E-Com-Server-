2a3f52b48d4aaec21f6093d98de39e492492a9c2
chinthaka senarathna <chironprabath@gmail.com>
2024-10-24 18:59:13 +0530

    CommandLineRunner ???

--- src/main/java/com/example/E_Com/EComApplication.java
+++ src/main/java/com/example/E_Com/EComApplication.java
@@ -1,13 +1,28 @@
 package com.example.E_com;
 
+import com.example.E_com.service.UserRoleService;
+import com.example.E_com.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EComApplication {
+public class EComApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EComApplication.class, args);
 	}
 
+	@Autowired
+	private UserRoleService userRoleService;
+
+	@Autowired
+	private UserService userService;
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRoleService.initializerUserRoles();
+		userService.initializeAdmin();
+	}
 }
